a9923dff34bc8b00e85c71cb9e0f6203fba5d07b
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-25 15:58:48 +0800

    color

--- src/15.2/scatter_squares.py
+++ src/15.2/scatter_squares.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 x_values = list(range(1, 1001))
 y_values = [i**2 for i in range(1, 1001)]
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,
+            edgecolor='none', s=40)
 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
