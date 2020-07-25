34b44a81fd542bbd6fc62f0a6a8ec7b142c62392
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-26 06:24:07 +0800

    frequency

--- src/chapter_15/die_visual.py
+++ src/chapter_15/die_visual.py
@@ -7,4 +7,9 @@ for roll_num in range(100):
     result = die.roll()
     results.append(result)
 
-print(results)
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
