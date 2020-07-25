d691337ed0aaf94fd99f62fb3ad0b3ee79c94994
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-25 15:33:05 +0800

    scatter_squares

--- src/15.2/scatter_squares.py
+++ src/15.2/scatter_squares.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=200)
+x_values = list(range(1, 1001))
+y_values = [i**2 for i in range(1, 1001)]
+plt.scatter(x_values, y_values, s=40)
 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of value", fontsize=14)
 
 plt.tick_params(axis='both', which='major', labelsize=14)
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
