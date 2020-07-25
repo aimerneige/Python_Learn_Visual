fd8c0efb3718cd838f418a47a2be87f0db379e23
AimerNeige <aimer.neige.soft@gmail.com>
2020-07-25 15:21:08 +0800

    mpl_squares

---
+++ src/15.2/mpl_squares.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
+
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of value", fontsize=14)
+
+plt.tick_params(axis='both', labelsize=14)
+plt.show()
