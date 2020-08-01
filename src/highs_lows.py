5b1cea89837cb69a52de280a30bff0bc65b27f0a
AimerNeige <aimer.neige.soft@gmail.com>
2020-08-01 11:37:04 +0800

    read number data

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -5,5 +5,9 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
